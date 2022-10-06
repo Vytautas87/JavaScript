52898017ce5d411c7a1c23869622b3706fc523df
Vytautas Izenbekas <vytautasizenbekas@Vytautass-MacBook-Air.local>
2022-10-06 09:47:10 +0300

    Sukurtas projektas

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("sveiki!");
+console.log("sveiki!");
+console.log("pasauli!");
+console.log("Pradedame mokytis Javascript");
+console.log("number 6");
+console.log(24 + 47);
+console.log("Namo numeris yra: 777")
+console.log("24" + "30");
+console.log(14 * 25);
+console.log(20 + 4 * 50);
+console.log(14 % 3);
+console.log(4 ** 5);
+console.log(+"30" + +"80");
+console.log("15" + 3 * 5);
+console.log(+"15" + 3 * 5) 

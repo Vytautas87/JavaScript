f73469a8699fe501786c58e15604f5a16318514f
Vytautas87 <vytautaszen1@gmail.com>
2022-10-06 11:59:46 +0300

    6

---
+++ script6.js
@@ -0,0 +1,17 @@
+
+    let a = prompt("Iveskite pirma skaiciu");
+    let b = prompt("iveskite antra skaiciu")
+    let z = a * b;
+    console.log(z)
+
+
+let x = 3 * 8;
+    console.log("plotas yra " + x);
+let x1 = 25 * 7;
+    console.log("plotas yra " + x1);
+let x2 = 3.148 * 3.5456;
+    console.log("plotas yra " + x2);
+    
+    function staciakampioPlotas(a,b) {
+        return (a*b); 
+    }

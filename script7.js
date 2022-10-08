1fd2ff8cba80150a56279f5b9f924a58e11b1409
Vytautas87 <vytautaszen1@gmail.com>
2022-10-08 10:05:19 +0300

    7

---
+++ script7.js
@@ -0,0 +1,9 @@
+let a = prompt("Įveskite skaiciu");
+let skaiciusA = skaicius(a );
+console.log(skaiciusA);
+
+// Papildomų funkcijų aprašymas
+function skaicius(a) {
+    let skaiciusA = a * -1;
+    return skaiciusA;
+}

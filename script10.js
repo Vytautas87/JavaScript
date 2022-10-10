f4108f97968c136847dd1c056d9f87240cb1679b
Vytautas87 <vytautaszen1@gmail.com>
2022-10-10 21:12:06 +0300

    statinio suradimas

--- script10.js
+++ script10.js
@@ -1,11 +1,11 @@
-let statinisb = prompt("Įveskite stačiojo trikampio statinio ilgį");
 let istrizainec = prompt("Įveskite stačiojo trikampio įstrižainės ilgį");
+let statinisb = prompt("Įveskite stačiojo trikampio statinio ilgį");
 let statinisa = statinioradimas(statinisb, istrizainec);
 console.log("statinis a " + statinisa);
 
 // Papildomos funkcijos
 function statinioradimas(a, b) {
-    return Math.sqrt(pakeltiKvadratu(a)- pakeltiKvadratu(b));
+    return Math.sqrt(pakeltiKvadratu(a) - pakeltiKvadratu(b));
 }
 
 function pakeltiKvadratu(a) {

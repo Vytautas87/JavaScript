d68875de4817fbb7e33d4ab6c81a1eb847760912
Vytautas87 <vytautaszen1@gmail.com>
2022-10-11 14:27:26 +0300

    apskritimo plotas

---
+++ pi.js
@@ -0,0 +1,7 @@
+let spindulioIlgis = +prompt("Įveskite apskritimo spindulį norėdami sužinoti apskritimo plotą");
+let pir2 = apskritimoPlotas(spindulioIlgis);
+console.log("Apskritimo plotas yra:" +pir2);
+
+function apskritimoPlotas(a) {
+    return Math.PI * a ** 2;
+}
